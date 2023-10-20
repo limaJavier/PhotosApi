6cd2b740e8f1b96d5253517375563292db0514e9
Javier Lima García <javi.limashelby@gmail.com>
2023-10-20 10:18:54 -0400

    Defining endpoints

---
+++ PhotosApi/Controllers/PhotosController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotosApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PhotosController : ControllerBase
+{
+    // GET: api/<PhotosController>
+    [HttpGet]
+    public IEnumerable<string> GetAllPhotos()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<PhotosController>/5
+    [HttpGet("{id}")]
+    public string GetPhoto(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<PhotosController>
+    [HttpPost]
+    public void StorePhoto([FromBody] string value)
+    {
+    }
+
+    // PUT api/<PhotosController>/5
+    [HttpPut("{id}")]
+    public void ChangePhoto(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<PhotosController>/5
+    [HttpDelete("{id}")]
+    public void DeletePhoto(int id)
+    {
+    }
+}
