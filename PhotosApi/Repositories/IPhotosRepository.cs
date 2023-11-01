f28c29809b7fea0362516a1461f9a525f082d8f9
Javier Lima <javi.limashelby@gmail.com>
2023-10-31 23:47:54 -0400

    IPhotosRepository

---
+++ PhotosApi/Repositories/IPhotosRepository.cs
@@ -0,0 +1,11 @@
+﻿using PhotosApi.Models;
+
+namespace PhotosApi.Repositories;
+
+public interface IPhotosRepository
+{
+    void Store(Guid id, Photo photo);
+    void Get(Guid id);
+    void Upsert(Guid id, Photo photo);
+    void Delete(Guid id);
+}
