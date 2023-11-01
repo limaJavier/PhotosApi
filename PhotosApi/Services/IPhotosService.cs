b6947f5cfc4bfc2e1b2c77773047d891960eadc3
Javier Lima <javi.limashelby@gmail.com>
2023-11-01 00:16:23 -0400

    IPhotosService and it's implementation

---
+++ PhotosApi/Services/IPhotosService.cs
@@ -0,0 +1,11 @@
+﻿using PhotosApi.Models;
+
+namespace PhotosApi.Services;
+
+public interface IPhotosService
+{
+    void StorePhoto(Photo photo);
+    Photo GetPhoto(Guid id);
+    void UpsertPhoto(Photo photo);
+    void DeletePhoto(Guid id);
+}
