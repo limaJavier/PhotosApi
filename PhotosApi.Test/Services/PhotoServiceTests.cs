aee25b6881aa209cbacd4912e5b374a711ac2a23
Javier Lima <javi.limashelby@gmail.com>
2023-11-10 09:26:00 -0500

    Done with PhotoServiceTests

--- PhotosApi.Test/Services/PhotoServiceTests.cs
+++ PhotosApi.Test/Services/PhotoServiceTests.cs
@@ -56,14 +56,89 @@ public class PhotosServiceTests
         repositoryMock.Verify(x => x.Store(photo), Times.Exactly(1));
     }
 
+    [Fact]
+    public void UpsertPhoto_ValidCall()
+    {
+        var photoFaker = GetPhotoFaker();
+
+        foreach (var photo in photoFaker.Generate(100))
+        {
+            // Arrange
+            var repositoryMock = new Mock<IPhotosRepository>();
+            repositoryMock.Setup(x => x.Upsert(photo));
+
+            var service = new PhotosService(repositoryMock.Object);
+
+            // Act
+            service.UpsertPhoto(photo);
+
+            // Assert
+            repositoryMock.Verify(r => r.Upsert(photo), Times.Exactly(1));
+        }
+
+    }
+
+    [Fact]
+    public void DeletePhoto_ValidCall()
+    {
+        var photoFaker = GetPhotoFaker();
+
+        foreach (var photo in photoFaker.Generate(100))
+        {
+            // Arrange
+            var repositoryMock = new Mock<IPhotosRepository>();
+            repositoryMock.Setup(x => x.Delete(photo.Id));
+
+            var service = new PhotosService(repositoryMock.Object);
+
+            // Act
+            service.DeletePhoto(photo.Id);
+
+            // Assert
+            repositoryMock.Verify(r => r.Delete(photo.Id), Times.Exactly(1));
+        }
+
+    }
+
+    [Fact]
+    public void GetPhotos_ValidCall()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            // Arrange
+            BuildRepo();
+            var expected = _repo.GetAll();
+            var repositoryMock = new Mock<IPhotosRepository>();
+            repositoryMock.Setup(r => r.GetAll()).Returns(() => expected);
+            var service = new PhotosService(repositoryMock.Object);
+
+            // Act
+            var actual = service.GetPhotos();
+
+            // Assert
+            Assert.True(expected.Length == actual.Length);
+
+            for (int j = 0; j < expected.Length; j++)
+            {
+                Assert.Equal(expected[i].Name, actual[i].Name);
+                Assert.Equal(expected[i].Id, actual[i].Id);
+                Assert.Equal(expected[i].Description, actual[i].Description);
+            }
+        }
+    }
+
     private void BuildRepo()
     {
-        var photoModelFake = new Faker<Photo>("es") // By default it's "en" English
+        var photoModelFaker = GetPhotoFaker();
+        foreach (var photo in photoModelFaker.Generate(100))
+            _repo.Store(photo);
+    }
+    private Faker<Photo> GetPhotoFaker()
+    {
+        var photoModelFaker = new Faker<Photo>("es") // By default it's "en" English
             .RuleFor(p => p.Id, Guid.NewGuid())
             .RuleFor(p => p.Name, f => f.Name.FirstName())
             .RuleFor(p => p.Description, f => f.Lorem.Text());
-
-        foreach (var photo in photoModelFake.Generate(100))
-            _repo.Store(photo);
+        return photoModelFaker;
     }
 }
